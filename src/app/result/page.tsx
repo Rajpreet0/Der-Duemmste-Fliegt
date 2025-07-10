ae01d6b35b107c1682a3b58ab1a4b41cb9c7e8f7
Rajpreet0 <rajpreet.singh@gmx.de>
2025-07-11 01:59:16 +0200

    fix deployment

--- src/app/result/page.tsx
+++ src/app/result/page.tsx
@@ -1,10 +1,15 @@
 "use client";
 
 import WinnerResult from "@/components/WinnerResult";
+import { Suspense } from "react";
 
 const Result = () => {
 
-  return <WinnerResult/>;
+  return (
+    <Suspense fallback={<p>Loading...</p>}>
+        <WinnerResult/>;
+    </Suspense>
+  ) 
 }
 
 export default Result
