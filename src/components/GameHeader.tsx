2bf8d75b76582f5c4d69455de38f6bd2dbccb625
Rajpreet0 <rajpreet.singh@gmx.de>
2025-07-10 20:00:24 +0200

    added basic functionality

--- src/components/GameHeader.tsx
+++ src/components/GameHeader.tsx
@@ -1,13 +1,16 @@
 import Image from "next/image";
 import Logo from "../../public/images/Logo.png";
-import { Heart, HeartOff, Settings, SkipForward } from "lucide-react";
+import { Heart, Settings, SkipForward } from "lucide-react";
 import TooltipComponent from "./TooltipComponent";
 
 interface GameHeaderProps {
   isSetup?: boolean;
+  currentPlayer?: string;
+  currentPlayerLives?: number;
+  onSkip?: () => void;
 }
 
-const GameHeader: React.FC<GameHeaderProps> = ({isSetup}) => {
+const GameHeader: React.FC<GameHeaderProps> = ({isSetup, currentPlayer, currentPlayerLives, onSkip}) => {
   return (
     <div className="w-full flex flex-row items-center justify-between p-2">
       <Image
@@ -18,18 +21,20 @@ const GameHeader: React.FC<GameHeaderProps> = ({isSetup}) => {
       />
       {!isSetup && (
         <div className="flex flex-col items-center justify-center">
-          <p className="text-3xl tracking-wider text-blue">Rajpreet</p>
+          <p className="text-3xl tracking-wider text-blue">{currentPlayer}</p>
           <div className="flex gap-2 items-center mt-1">
-            <Heart className="text-red w-5" fill="#D74A37"/>
-            <Heart className="text-red w-5" fill="#D74A37"/>
-            <Heart className="text-red w-5" fill="#D74A37"/>
+            {Array.from({length: currentPlayerLives ?? 0}).map((_, idx) => (
+              <Heart key={idx} className="text-red w-5" fill="#D74A37"/>
+            ))}
           </div>
         </div>
       )}
       <div className="flex gap-4 items-center">
         {!isSetup && (
           <TooltipComponent content="Frage überspringen">
-            <div className="bg-blue p-2 rounded-full text-white cursor-pointer hover:scale-105 transition-all">
+            <div
+              onClick={onSkip} 
+              className="bg-blue p-2 rounded-full text-white cursor-pointer hover:scale-105 transition-all">
                 <SkipForward/>
             </div>
           </TooltipComponent>
